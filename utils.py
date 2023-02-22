4460b7e9072e46e3c08d4a11ed98ae20041d0dde
Brian Zanfelice <69322400+brian-zanfelice@users.noreply.github.com>
2023-02-22 16:29:55 -0300

    new game state class (#1)

---
+++ utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+class game_state:
+    def __init__(self, x, y, spikes_matrix) -> None:
+        self.x = x
+        self.y = y
+        self.previous_x = x
+        self.previous_y = y
+        self.spikes_matrix = spikes_matrix
+
+    def save_state(self, jump: bool):
+        data = {
+            "X": [],
+            "Y": [],
+            "Previous_X": [],
+            "Previous_Y": [],
+            "Spike_0": [],
+            "Spike_1": [],
+            "Spike_2": [],
+            "Spike_3": [],
+            "Spike_4": [],
+            "Spike_5": [],
+            "Spike_6": [],
+            "Spike_7": [],
+            "Spike_8": [],
+            "Spike_9": [],
+            "Spike_10": [],
+            "Spike_11": [],
+            "Jump": [],
+        }
+        data["X"].append(self.x)
+        data["Y"].append(self.y)
+        data["Previous_X"].append(self.previous_x)
+        data["Previous_Y"].append(self.previous_y)
+        for num, spike in enumerate(self.spikes_matrix):
+            data[f"Spike_{num}"].append(spike)
+        data["Jump"].append(jump)
+        save_data = pd.DataFrame(data)
+        save_data.to_csv("data.csv", mode="a", header=False, index=False)
